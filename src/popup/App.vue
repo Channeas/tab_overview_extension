b7e79565305954db50ad8c287d3cecefc83aa2e1
Johannes <johannes.idarsson@gmail.com>
2020-07-31 13:54:30 +0200

    initial commit

---
+++ src/popup/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="popup"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style scoped>
+#popup {
+    height: 200px;
+    width: 350px;
+}
+</style>
